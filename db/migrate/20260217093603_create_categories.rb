7f89cafae2f7e33ed70416dc16d6abbae4efe801
vishnumangu22 <vishnumangu22@gmail.com>
2026-03-23 09:39:34 +0530

    Committed Succesfully

--- db/migrate/20260217093603_create_categories.rb
+++ db/migrate/20260217093603_create_categories.rb
@@ -7,3 +7,4 @@ class CreateCategories < ActiveRecord::Migration[7.2]
     end
   end
 end
+  
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,9 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  test "category should be valid" do
+    @category = Category.new(name: "Sports")
+    assert @category.valid?
+  end
 end
